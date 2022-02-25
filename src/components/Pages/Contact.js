4e0c090dda62cf4428b9a7c445c246e072b1a6b7
Gabe L'Herault <glherault@tcmsolutions.net>
2022-02-24 19:32:58 -0600

    Add download links

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -4,7 +4,6 @@ import Slide from '@mui/material/Slide';
 import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
-import doc from '../../static/documents/'
 import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 
 export default function Contact() {
@@ -29,33 +28,35 @@ export default function Contact() {
   // Set the drag hook and define component movement based on gesture data.
 
 
-  return ( 
-   
-    <Box display='flex' flexDirection='column' maxHeight='95vh' maxWidth='95vw' sx={{ alignItems: 'center', justifyContent: 'center'}}>
-     <Button variant="outlined" onClick={contactDisplay} sx={{marginTop: 4}}>Contact Me</Button>
-     <Card sx={{
-      maxWidth: {xs:300, md: 350, lg: 400},
-      bgcolor: 'rgba(82,168,251,0.00)',
-      display: displayContact,
-      flexDirection: 'column',
-      padding: 1,
-     }}>
-       <Slide direction="left" in={checked} mountOnEnter unmountOnExit>
-        <Button variant="outlined" sx={{marginTop: 2}}><Link href="mailto:glherault@tcmsolutions.net">Email</Link></Button>
+  return (
+
+    <Box display='flex' flexDirection='column' maxHeight='95vh' maxWidth='95vw' sx={{ alignItems: 'center', justifyContent: 'center' }}>
+      <Button variant="outlined" onClick={contactDisplay} sx={{ marginTop: 4 }}>Click Me</Button>
+      <Card sx={{
+        maxWidth: { xs: 300, md: 350, lg: 400 },
+        bgcolor: 'rgba(82,168,251,0.00)',
+        display: displayContact,
+        flexDirection: 'column',
+        padding: 1,
+      }}>
+        <Slide direction="left" in={checked} mountOnEnter unmountOnExit>
+          <Button variant="outlined" sx={{ margin:2 }}><Link href="mailto:glherault@tcmsolutions.net">Email</Link></Button>
         </Slide>
 
         <Slide direction="right" in={checked} mountOnEnter unmountOnExit>
-        <Button variant="outlined" sx={{marginTop: 2}}><ArrowCircleDownIcon/><Link href="../../static/documents/Gabe L'Herault - FS Resume 2022.docx">Email</Link></Button>
+          <Box sx={{margin: 1}}>
+            <ArrowCircleDownIcon sx={{ color: '#e3ef30' }}/><Link href="../../static/documents/Gabe L'Herault - FS Resume 2022.docx" download>Download Resume DOC</Link>
+          </Box>
         </Slide>
 
         <Slide direction="left" in={checked} mountOnEnter unmountOnExit>
-        <Button variant="outlined" sx={{marginTop: 2}}>Email</Button>
+          <Box sx={{ margin: 1}}>
+            <ArrowCircleDownIcon sx={{ color: '#e3ef30'}} /><Link href="../../static/documents/Gabe L'Herault - FS Resume 2022.pdf" download>Download Resume PDF</Link>
+          </Box>
         </Slide>
-      
-      
-     </Card>
+      </Card>
     </Box>
-    
+
 
 
   );
